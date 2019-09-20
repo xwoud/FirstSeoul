82aeb03ee5e026a07a88e5af339595939162d09c
민희💕 <mini@gimminhuiui-macbook-pro.local>
2019-09-20 15:28:56 +0900

    Initial Commit

---
+++ FirstSeoul/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirstSeoul
+//
+//  Created by 김민희 on 2019. 9. 20..
+//  Copyright © 2019년 김민희. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
